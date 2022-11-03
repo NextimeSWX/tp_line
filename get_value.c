09819027602a6f07bf054bed00a6dd39ad278633
Nextime <evan.lebihan@ecole-89.com>
2022-11-03 10:38:42 +0100

    Valeur Ratio

---
+++ get_value.c
@@ -0,0 +1,14 @@
+float get_value(int a, int b, float r) {
+    float x;
+    float i;
+
+    if(b > a) {
+    i = b - a;
+    x = a + (i * r);
+    }
+    else {
+        i = a - b;
+        x = b + (i * r);
+    }
+    return(x);
+}
