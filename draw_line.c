38488e727c6892cf8f4df7704d2d14211b5928aa
Nextime <evan.lebihan@ecole-89.com>
2022-11-08 16:34:59 +0100

    Draw line

--- draw_line.c
+++ draw_line.c
@@ -10,6 +10,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+float get_ratio(int a, int b, int x);
+float get_value(int a, int b, float r);
 void put_pixel(t_bunny_position *pos,
                t_bunny_pixelarray *px,
                unsigned int color);
@@ -19,11 +21,20 @@ void stu_draw_line(t_bunny_position *pos_a,
                    t_bunny_pixelarray *px,
                    unsigned int color)
 {
-    t_bunny_position *pos;
+    t_bunny_position pos;
+    float r;
+    float x;
+    int z;
 
-    pos = pos_a;
-    while(pos -> x <= pos_b -> x) {
-        put_pixel(pos, px, color);
-        pos -> x = pos -> x + 1;
-    }
+    pos.x = pos_a -> x;
+    pos.y = pos_a -> y;
+    z = pos_a -> x;
+    
+     while(pos_b -> x >= z) {
+         put_pixel(&pos, px, color);
+         r = get_ratio(pos_a -> x, pos_b -> x, z);
+         pos.y = get_value(pos_a -> y, pos_b -> y, r);
+         pos.x = pos.x + 1;
+         z = z + 1;
+         }
 }
