773869190342739f28191c7aa6a67dc425b5c66a
Evan Lebihan <evan.lebihan@ecole-89.com>
2022-11-10 15:02:15 +0100

    Update 'get_ratio.c'

--- get_ratio.c
+++ get_ratio.c
@@ -1,12 +1,12 @@
-float get_ratio(int a, int b, int x) {
+float get_ratio(int a, int b, int x)
+{
     float ratio;
     float i;
 
     if(b > a) {
         i = b - a;
         ratio = (x - a) / i;
-    }
-    else {
+    } else {
         i = a - b;
         ratio = (x - b) / i;
     }
