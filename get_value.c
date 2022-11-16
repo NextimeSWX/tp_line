8cb458c55cb24232314a2af72dad7c97f887210b
Evan Lebihan <evan.lebihan@ecole-89.com>
2022-11-16 09:33:32 +0100

    Update 'get_value.c'

--- get_value.c
+++ get_value.c
@@ -1,3 +1,12 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     Get_value
+ * created on:  2022-11-03 - 11:13 +0100
+ * 1st author:  evan.lebihan - evan.lebihan
+ * description: get_value.c
+ */
+
+
 int get_value(int a, int b, float r)
 {
     float x;
