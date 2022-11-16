111ad6364944d9acfb3fc54ef05e8ec87e05d468
Evan Lebihan <evan.lebihan@ecole-89.com>
2022-11-16 09:32:51 +0100

    Update 'get_ratio.c'

--- get_ratio.c
+++ get_ratio.c
@@ -1,3 +1,12 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     get_ratio
+ * created on:  2022-11-03 - 10:13 +0100
+ * 1st author:  evan.lebihan - evan.lebihan
+ * description: get_ratio.c
+ */
+
+
 float get_ratio(int a, int b, int x)
 {
     float r;
